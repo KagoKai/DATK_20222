33b4fa3c8b725d01b106cf99dd252673ebba4764
Le Huy Hieu <kagokai123@gmail.com>
2023-07-12 15:12:33 +0700

    Added WebSocket service.

---
+++ NodeServer/config/express.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const ejs = require('ejs');
+const path = require('path');
+
+module.exports.init = function(app)
+{
+
+    app.use(express.static('public'));
+    app.set('views', path.join(__dirname, '../views'));
+    app.set('view engine', 'ejs');
+
+    let ejsOptions = {outputFunctionName: 'echo'};
+    app.engine('ejs', (path, data, cb) => {
+        ejs.renderFile(path, data, ejsOptions, cb);
+    });
+
+
+}
---
+++ NodeServer/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = 8080;
+
+const express_config = require('./config/express.js');
+
+express_config.init(app);
+
+var cameraArray={};
+
+app.get('/', (req, res) => {
+    res.render('index', {});
+});
+
+app.get('/video', (req, res) => {
+    res.render('video', {});
+});
+
+app.get('/live', (req, res) => {
+    res.render('live', {});
+});
+
+io.on('connection', (socket) => {
+
+  const listener = (eventName, ...args) => {
+    console.log(eventName, args);
+  }
+  
+  socket.onAny(listener);
+
+  console.log(`Connected to client ${socket.id}`)
+
+  socket.on('jpgstream_serverio', (msg) => {
+    console.log('Received image')
+    io.to('webusers').emit('jpgstream_client', msg.pic);
+  });
+  
+  socket.on('webuser', (msg) => {
+    console.log('Connected to website')  
+    socket.join('webusers');      
+  });
+});
+
+http.listen(port, () => {
+      console.log(`App listening at http://localhost:${port}`)
+})
